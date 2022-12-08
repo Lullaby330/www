852515e217805dd9211f4f070d093ea4b1120afa
Lullaby330 <69coder@gmail.com>
2022-12-08 10:35:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -37,12 +37,7 @@ except ValueError:
 
 #Force sub message 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
-
-Note :
-Jangan spam ya karna bisa membuat bot delayed.
-Sekian dan Terima Kasih.")
-
-#set your Custom Caption here, Keep None for Disable Custom Caption
+ your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
